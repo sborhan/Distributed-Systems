633401316ee1e904f7cf1482238a0cbdc1363c97
Sam Borhan <55805065+sborhan@users.noreply.github.com>
2020-10-19 13:26:35 -0700

    Update Oct-19-2020

--- lab3.py
+++ lab3.py
@@ -1,15 +1,16 @@
 import datetime
 import sys
 import socket
-
 import fxp_bytes_subscriber
-
 import bellman_ford
 
 MY_ADDRESS = ('127.0.0.1', 55555)
 MY_Byte_ADDRESS = b'\x7f\x00\x00\x01\xd9\x03'
-TIME_OUT = 3
+TIME_OUT = 600  # time out after 10 minutes
 BUFF_SIZE = 1024
+LIFETIME = 1.5
+ROOT = 'USD'
+VERTICES_NUMBER = 50
 
 
 class Lab3(object):
@@ -25,65 +26,59 @@ class Lab3(object):
         self.my_graph = None
         self.old_date = None
         self.black_list = []
+        self.my_graph = bellman_ford.Graph(VERTICES_NUMBER)
 
-    def get_udp_message(self):
-        self.listener.settimeout(TIME_OUT)
+    def run(self):
+
         self.listener.sendto(self.byte_address, self.provider)
-        try:
-            my_graph = bellman_ford.Graph(100)
-            while True:
-                # for _ in range(1):
-                #     print(' ')
+        while True:
+            print('\nWaiting {} minutes to receive all UDP '
+                  'messages....\n'.format(int(TIME_OUT / 60)))
+            try:
+                self.listener.settimeout(TIME_OUT)
                 data, server = self.listener.recvfrom(BUFF_SIZE)
-                # print('Receiving UDP Message from {}'.format(server))
-                packet = [data[i:i + 32] for i in range(0, len(data), 32)]
-                # print('This packet has {} info'.format(len(packet)))
+            except socket.timeout:
+                print('REQUEST TIMED OUT AFTER {} SECONDS'.format(TIME_OUT))
+                print('Socket Closed : No More Data Received!')
+                self.listener.close()
+                break
 
-                for (c1, c2) in self.received_time.keys():
-                    received_time = self.received_time.get((c1,
-                                                            c2))
-                    if (datetime.datetime.utcnow() -
-                        received_time).seconds > 1.5:
-                        # self.receiving_time.pop((c1, c2))
-                        self.black_list.append((c1, c2))
-                        print('removing stale quote for ({}, {})'.format(
-                            c1, c2))
-                        # my_graph.RemoveEdge(currency_1, currency_2)
-                        my_graph.RemoveEdge(c1, c2)
-                        continue
-                while self.black_list:
-                    del self.received_time[self.black_list.pop()]
+            self.check_current_data_lifetime()
+            self.remove_old_data()
+            # print('Receiving UDP Message from {}'.format(server))
+            packet = [data[i:i + 32] for i in range(0, len(data), 32)]
+            self.add_data_to_my_graph(packet)
+            self.my_graph.BellmanFord(ROOT)
 
-                for data in packet:
-                    current_date, currency_1, currency_2, exchange_rate = \
-                        Lab3.get_info(self, data)
-                    print('{} {} {} {}'.format(current_date,
-                                               currency_1,
-                                               currency_2,
-                                               exchange_rate))
+    def add_data_to_my_graph(self, packet):
+        for data in packet:
+            current_date, currency_1, currency_2, exchange_rate = \
+                self.get_info(data)
+            print('{} {} {} {}'.format(current_date, currency_1, currency_2,
+                                       exchange_rate))
+            if self.old_date is None:
+                self.old_date = current_date
+            elif current_date < self.old_date:
+                print('ignoring out-of-sequence message')
+                continue
+            else:
+                self.old_date = current_date
 
-                    if self.old_date is None:
-                        self.old_date = current_date
+            self.received_time[(currency_1, currency_2)] = current_date
+            self.my_graph.add_edge(currency_1, currency_2, exchange_rate)
 
-                    if current_date < self.old_date:
-                        print('ignoring out-of-sequence message')
-                        continue
-                    else:
-                        self.old_date = current_date
+    def check_current_data_lifetime(self):
+        for (currency_1, currency_2) in self.received_time.keys():
+            received_time = self.received_time.get((currency_1, currency_2))
+            if (datetime.datetime.utcnow() - received_time).seconds > LIFETIME:
+                self.black_list.append((currency_1, currency_2))
+                self.my_graph.RemoveEdge(currency_1, currency_2)
+                print('removing stale quote for ({}, {})'.format(currency_1,
+                                                                 currency_2))
 
-                    self.received_time[(currency_1, currency_2)] = \
-                        current_date
-
-                    my_graph.add_edge(currency_1, currency_2, exchange_rate)
-
-                    # lab3.print_currencies(self)   # use as a test message
-
-                my_graph.BellmanFord('USD')
-        except socket.timeout:
-            print('REQUEST TIMED OUT AFTER {} SECONDS'.format(TIME_OUT))
-
-        print('Socket Closed : No More Data Received!')
-        self.listener.close()
+    def remove_old_data(self):
+        while self.black_list:
+            del self.received_time[self.black_list.pop()]
 
     def get_info(self, data):
         current_date = fxp_bytes_subscriber.get_date(data[0:8])
@@ -115,28 +110,35 @@ class Lab3(object):
         return listener, listener.getsockname()
 
 
-if __name__ == '__main__':
+def check_input(input):
+    port = None
+    host = None
 
-    if len(sys.argv) == 2 or len(sys.argv) > 3:
+    if len(input) == 2 or len(input) > 3:
         print("Usage: python3 lab3.py [Provider_HOST] [Provider_PORT] ")
-        exit(1)
 
-    elif len(sys.argv) == 1:
+    elif len(input) == 1:
         host = 'localhost'
         port = 50405
 
     else:
         try:
-            port = int(sys.argv[2])
+            port = int(input[2])
         except ValueError:
             print("Oops! That was no valid Port number.  Try again...")
             exit(1)
-        host = sys.argv[1]
 
-    if port not in range(1025, 65536):
-        print("Port needs to be between 1025-65535.")
-        exit(1)
+        if port not in range(1025, 65536):
+            print("Port needs to be between 1025-65535.")
+            exit(1)
 
+        host = input[1]
+
+    return host, port
+
+
+if __name__ == '__main__':
+    host, port = check_input(sys.argv)
     provider_address = (host, port)
     lab3 = Lab3(provider_address)
-    lab3.get_udp_message()
+    lab3.run()
